9babbc56217088bddebe4af250086b0962500d17
veliValentine <nicolas.valentine@helsinki.fi>
2020-10-18 23:59:07 +0300

    10.14

---
+++ src/utils/authStorage.js
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    const rawToken = await AsyncStorage.getItme(
+      `${this.namespace}:token`,
+    );
+
+    return rawToken ? JSON.parse(rawToken): [];
+  }
+
+  async setAccessToken(accessToken) {
+    // to remove previous token from storage
+    this.removeAccessToken();
+
+    await AsyncStorage.setItem(
+      `${this.namespace}:token`,
+      JSON.stringify(accessToken),
+    );
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
+  }
+}
+
+export default AuthStorage;
